825769bfa0f64956fff91c71bb15b4909240c3fa
Allison Kim <akeem159@yahoo.com>
2022-08-31 14:36:38 -0400

    commit tree ea969ea23f8c0d3a64a84e2116e91b7ebd371345

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,26 @@
 class Triangle
-  # write code here
+  def initialize(one, two, three)
+    @one = one
+    @two = two
+    @three = three
+  end
+
+  def kind
+    one = @one
+    two = @two
+    three = @three
+
+    if one > 0 && two > 0 && three > 0 && one + two > three && two + three > one && one + three > two
+      if one == two && two == three
+        :equilateral
+      elsif one == two || two == three || one == three
+        :isosceles
+      else :scalene
+      end
+    else raise TriangleError
+    end
+  end
+
+  class TriangleError < StandardError
+  end
 end
